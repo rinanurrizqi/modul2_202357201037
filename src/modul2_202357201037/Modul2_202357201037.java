6191c262250a531c19f4fd2412dbdaba18322754
rinanurrizqi <rinanurrizqi@gmail.com>
2024-03-28 20:56:58 +0700

    Exception e

--- src/modul2_202357201037/Modul2_202357201037.java
+++ src/modul2_202357201037/Modul2_202357201037.java
@@ -15,6 +15,21 @@ public class Modul2_202357201037 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int bil = 10;
+        String b[] = {"a", "b", "c"};
+        try {
+            System.out.println(bil / 0);
+            System.out.println(b[3]);
+        } catch (ArithmeticException ai) {
+            System.out.println("Error Aritmetik");
+            System.out.println(ai.getMessage());
+        } catch (ArrayIndexOutOfBoundsException n) {
+            System.out.println("Error karena melebihi array");
+            System.out.println(n.getMessage());
+        } catch (Exception e) {
+            System.out.println("Ada Error");
+            System.out.println(e.getMessage());
+        }
     }
     
 }
